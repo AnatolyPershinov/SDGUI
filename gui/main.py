e26072d6aff1a7edbe11f551c74ff16617922587
Anatoly <anatoliypershinov@gmail.com>
2024-01-23 01:06:54 +0300

    init

---
+++ gui/main.py
@@ -0,0 +1,31 @@
+import flet as ft
+from models.SD import SDModel
+
+
+model = SDModel()
+
+def main(page: ft.Page):
+    text_input1 = ft.TextField(hint_text="Prompt", multiline=True)
+    text_input2 = ft.TextField(hint_text="Negative prompt", multiline=True)
+    send_button = ft.ElevatedButton(text="Generate")
+
+    def send_click(e):
+        img = model.query(text_input1.value, text_input2.value)
+        
+
+    send_button.on_click = send_click
+
+    image = ft.Image(src="/images/placeholder.svg", width=512, height=512)
+    input_container = ft.Container(ft.Column([text_input1, text_input2, send_button]), padding=100)
+    
+
+    container = ft.Row([
+        input_container,
+        image
+        
+    ])
+
+    page.add(container)
+
+
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
---
+++ models/SD.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+from diffusers import DiffusionPipeline
+import torch
+
+
+class Model(ABC):
+    def __init__(self, cuda=True ):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def query(self, prompt, negative_prompt=None):
+        raise NotImplementedError
+
+
+class SDModel(Model):
+    def __init__(self, cuda=True):
+        self.pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", 
+                                                      torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+        if cuda:
+            self.pipe.to('cuda')
+
+    def query(self, prompt, negative_prompt=None):
+        return self.pipe(prompt=prompt, negative_prompt=negative_prompt).images[0]
